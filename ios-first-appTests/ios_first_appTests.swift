b288287f326e873e3f7d7675bd8a23cdef1378c1
Sunrock14 <farukkocademir@hotmail.com>
2024-12-26 23:17:06 +0300

    Add project files.

---
+++ ios-first-appTests/ios_first_appTests.swift
@@ -0,0 +1,17 @@
+//
+//  ios_first_appTests.swift
+//  ios-first-appTests
+//
+//  Created by İsmail Faruk Kocademir on 26.12.2024.
+//
+
+import Testing
+@testable import ios_first_app
+
+struct ios_first_appTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
